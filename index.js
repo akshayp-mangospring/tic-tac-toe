94f83bfe973fab2b81d60f1d9e54729797d0a9cf
akshayp-mangospring <akshayp@mangospring.com>
2022-07-05 16:49:41 +0530

    Add Tic Tac Toe in pure HTML, CSS, JS - Extensively uses browser variables and CSS functions - Scales according to viewport without the usage of media queries - Uses browser variables for scaling also deciding the size of board based on viewport

---
+++ index.js
@@ -0,0 +1,109 @@
+const getWinCombos = (() => {
+  const wc = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  return () => wc;
+})();
+
+const getDomElemFromStr = (str) => {
+  const domParser = new DOMParser();
+  const doc = domParser.parseFromString(str, 'text/html');
+
+  return doc.body.firstElementChild;
+};
+
+const hasPlayerWon = (gameCells, player) => {
+  const { marker } = player;
+  const winCombos = getWinCombos();
+
+  for (let i = 0; i < winCombos.length; i++) {
+    const winCombo = winCombos[i];
+    let matchCount = 0;
+
+    for (let j = 0; j < winCombo.length; j++) {
+      const elem = winCombo[j];
+      if (gameCells[elem] !== marker) break;
+      matchCount += 1;
+    }
+
+    if (matchCount === 3) return true;
+  }
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Freezing objects doesn't let anyone add or alter properties of an object after initiation thus adding security that our script might not be altered on the Front.
+  const gameState = (() => {
+    const gs = Array(9).fill(null);
+    return Object.freeze({
+      getCells: () => gs,
+      setCell: (i, v) => {
+        gs[i] = v;
+      },
+    });
+  })();
+  const oPlayer = Object.freeze({
+    marker: 'o',
+    struct: '<div class="marker o-marker"></div>'
+  });
+  const xPlayer = Object.freeze({
+    marker: 'x',
+    struct: '<div class="marker x-marker"></div>'
+  });
+
+  const gameBoard = document.getElementById('game-board');
+  let currentPlayer = xPlayer;
+
+  gameBoard.addEventListener('click', (e) => {
+    const elem = e.target;
+
+    //Don't perform action for already filled up cell or don't perform any action outside a cell
+    if (elem.classList.contains('filled-in') || !elem.classList.contains('cell')) return;
+
+    // Fill in the marker on DOM
+    elem.appendChild(getDomElemFromStr(currentPlayer.struct));
+
+    // Update the marker in Game state
+    gameState.setCell(
+      Array.prototype.indexOf.call(elem.parentNode.children, elem),
+      currentPlayer.marker
+    );
+    elem.classList.add('filled-in');
+
+    if (hasPlayerWon(gameState.getCells(), currentPlayer)) {
+      // Declare Winner
+      // Moving to async so as to let the DOM get painted before declaring a winner to the user.
+      setTimeout(() => {
+        alert(`${currentPlayer.marker.toUpperCase()} has Won the Game!`);
+        location.reload();
+      }, 0);
+
+      // This return is essential because the page doesn't reload until all the alerts are shown.
+      // Also to prevent execution of further code to increase performance.
+      return;
+    } else {
+      // Switch Turn
+      if (currentPlayer === xPlayer) {
+        currentPlayer = oPlayer;
+      } else {
+        currentPlayer = xPlayer;
+      }
+    }
+
+    if (gameState.getCells().every((el) => el !== null)) {
+      // Declare Tie
+      // Moving to async so as to let the DOM get painted before asking to play again.
+      setTimeout(() => {
+        alert("It's a tie! Play again!");
+        location.reload();
+      }, 0);
+    }
+  });
+});

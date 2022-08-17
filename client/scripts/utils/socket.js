c117b124617cc956a3911676ba229a5732ef551a
akshayp-mangospring <akshayp@mangospring.com>
2022-08-17 20:52:31 +0530

    Remove IIFE

--- client/scripts/utils/socket.js
+++ client/scripts/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 import { serverUrl } from '../constants';
 
-export const socket = (() => io.connect(serverUrl))();
+export const socket = io.connect(serverUrl);
 
 export default socket;
